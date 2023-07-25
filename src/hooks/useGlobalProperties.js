93229aa64f360d582adb5be4a235eb96ae0bdb6a
邵竞帆 <alanshao@192-168-1-130.tpgi.com.au>
2023-07-26 03:44:34 +1000

    封装一手自定义hook

--- src/hooks/useGlobalProperties.js
+++ src/hooks/useGlobalProperties.js
@@ -1,7 +1,7 @@
 import { getCurrentInstance, onMounted } from "vue"
 
 export default function(key) {
-    onMounted(() => { console.log('get fetchReq plugin') })
+    onMounted(() => { console.log(`get ${key} plugin`) })
     // 使用自定义插件
     const instance = getCurrentInstance();
     return instance.appContext.config.globalProperties[key];
